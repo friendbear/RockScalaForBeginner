05156b08edad85009f400e36b37dae9ea75487b9
T Kumagai <kumagai@bear.plala.or.jp>
2018-12-23 01:12:38 +0900

    ValuesVariables

---
+++ basics/ValuesVariablesTypes.scala
@@ -0,0 +1,30 @@
+package basics
+
+object Main extends App {
+
+  val x = 42
+  println(x)
+
+
+  // VALS ARE IMMUTABLE
+
+  // COMPILER can infer types
+  
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 52522525252225L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+
+  aVariable = 5 // side effects
+
+
+}
