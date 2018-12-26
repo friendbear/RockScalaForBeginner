217be91e53408361b53fc0c438e378d3eee6cdb8
T Kumagai <kumagai@bear.plala.or.jp>
2018-12-26 10:18:15 +0900

    object and class

--- oop/MethodNotations.scala
+++ oop/MethodNotations.scala
@@ -2,19 +2,26 @@
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, val favoriteMovie: String,val age: Int = 1) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favorite)
 
     def unary_! : String = s"$name, what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
 
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(time: Int): String = s"$name watched Inception $time times"
+    
+    
+    def learns(thing: String): String = s"$name learns $thing"
+    def learnsScala = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception")) // equivalent
-  println(mary likes "Inception") // equivalent
+  println(mary likes "Inception")   // equivalent
   // infix notation = operator notation (syntactic sugar)
 
   // "operators" in Scala
@@ -44,6 +51,32 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary())  // equivalent
+
+  /*
+   * 1. Overload the + operator
+   * mery + "the rockstar" => new Person "Mary (the rockstar)"
+   */
+  println((mary + "the rockstar").apply())
+
+  /*
+   * 2. Add an age to Person class
+   *    Add a unary + operator => new Person with the age + 1
+   *    +mary => marry with the age incrementer
+   */
+  println((+mary).age)
+
+  /*
+   * 3 Add a "learns" method in the Person class => "Mary learns Scala"
+   *   Add a learnsScala method, calls learns method with "Scala"
+   *   Use it in postfix notation
+   */
+  println(mary learns "Scala")
+
+  /*
+   * Overload the apply method
+   * marry.apply(2) => "Mary watched Inception 2 times"
+   */
+  println(mary(2))
 
 } 
 
---
+++ oop/Objects.scala
@@ -0,0 +1,36 @@
+
+
+object Objects {
+
+  // SCALA DOES NOT HAVE CLASS LEVEL FUNCTIONALITY ("static")
+ 
+  object Person { // type + its only instance
+    // "static"/"class" = level functionality
+    val N_EYES = 2
+    def canFly: Boolean = false
+
+    def apply(mother: Person, father: Person): Person = new Person("Bobble")
+  } 
+
+  class Person(val name: String) {
+    // instance-level functionally
+  }
+  //COMPANIONS
+
+  def main(args: Array[String]): Unit = {
+
+    println(Person.N_EYES)
+    println(Person.canFly)
+
+    // Scala object = SINGLETO INSTANCE
+    val mary = new Person("Mary")
+    val john = new Person("John")
+    println(mary == john) // => false
+
+    val person1 = Person
+    val person2 = Person
+    println(person1 == person2) // => true
+
+    val boble = Person(mary, john)
+  }
+}
