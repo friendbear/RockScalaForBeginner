64d0fd6c3e612c1338fb233a30bf09ec84d761e2
friendbear <kumagai@bear.plala.or.jp>
2019-01-12 12:24:14 +0900

    Functional Programing

--- src/main/scala/functionprograming/WhatsAFunction.scala
+++ src/main/scala/functionprograming/WhatsAFunction.scala
@@ -4,6 +4,11 @@ trait MyFunction[A, B] {
   def apply(element:A): B
 }
 
+/**
+  * trait Function1[-A, +B] {
+  *   def apply(element: A): B
+  * }
+  */
 object WhatsAFunction extends App {
 
   
@@ -24,21 +29,39 @@ object WhatsAFunction extends App {
     def apply(a: Int, b: Int): Int = a + b
   }
 
-  // Function types Function2[A, B, R] == (A, B) => B
+  // Function types Function2[A, B, R] == (A, B) => R
 
   // ALL SCALA FUNCTIONS ARE OBJECTS
 
   /*
    * 1. a function which takes 2 strings and concatenates them
    * 2. transform the MyPredicate and MyTransformer into function types
-   * 3 define a function which takes an int and returns another function which takes an int
-   *   - what's the type of 
+   * 3. define a function which takes an int and returns another function which takes an int
+   *   - what's the type of
+   *   - how to do it
+   * Use Syntax Sugar
    */
-
   def concatenator: (String, String) => String = new Function2[String, String, String] {
     override def apply(a: String, b: String): String = a + b
   }
+
   println(concatenator("Hello", "Scala"))
+
+  // Function1[Int, Function1[Int, Int]]
+  val superAdder: Function1[Int, Function1[Int, Int]] = new Function1[Int, Function1[Int, Int]]{
+    override def apply(x: Int): Function1[Int, Int] = new Function1[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+  val adder3 = superAdder(3)
+  println(adder3(4))
+  println(superAdder(3)(4)) // curried function
+
+
+  trait MyFunction[A, B] {
+    def apply(element: A): B
+  }
 
 }
 
