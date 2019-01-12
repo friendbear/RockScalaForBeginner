88cef74a5c6d6bef6d19adb511e853322a48a5a6
friendbear <kumagai@bear.plala.or.jp>
2019-01-12 12:37:28 +0900

    AnonymousFunction Basic

---
+++ src/main/scala/functionprograming/Anonymousfunctions.scala
@@ -0,0 +1,36 @@
+package functionprograming
+
+object Anonymousfunctions extends App {
+
+  val doublerF = new Function[Int, Int] {
+    override def apply(v1: Int): Int = v1 * 2
+  }
+
+  // anonymous function (LAMBDA)
+  val doubler1: Int => Int = (x: Int) => x * 2
+  val doubler2: Int => Int = x => x * 2
+  val doubler = (x: Int) => x * 2
+
+  // multiple params in a lambda
+  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  // no params
+  val justDoSomething: () => Int = () => 3
+
+  println(justDoSomething) // function itself
+  println(justDoSomething()) // call
+
+  // curly braces with lambdas
+  val stringToInt = { (str: String) =>
+    str.toInt
+  }
+
+  // MOAR syntactic sugar
+  val niceIncrementer: Int => Int = _ + 1
+  val niceAdder: (Int, Int) => Int  = _ + _ // equivalent to (a, b) => a + b
+
+  /*
+    1. MyList: replace all FunctionX calls with lambdas
+    2. Rewrite the "special" adder as an anonymous function
+   */
+}
