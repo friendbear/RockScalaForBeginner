882ede2d49bf27114633e3a74180fd7897d5a4c0
friendbear <kumagai@bear.plala.or.jp>
2019-01-13 10:59:14 +0900

    Add Test

--- src/test/scala/basics/FunctionsSpeck.scala
+++ src/test/scala/basics/FunctionsSpeck.scala
@@ -1,5 +1,10 @@
 package basics
 
-class FunctionsSpeck {
+import org.scalatest.{FlatSpec, Matchers}
+class FunctionsSpeck extends FlatSpec with Matchers{
 
+  "Functions" should "mix" in {
+    Functions.aRepeatedFunction("hoge", 3) should be ("hogehogehoge")
+    Functions.isPrime(4) should be (true)
+  }
 }
