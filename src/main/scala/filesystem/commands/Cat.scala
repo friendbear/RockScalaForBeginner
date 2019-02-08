9d1dfd30b31c40008b6604ab87b7abf5f1f0c5a7
friendbear <kumagai@bear.plala.or.jp>
2019-02-08 09:47:40 +0900

    scala spiral

---
+++ src/main/scala/filesystem/commands/Cat.scala
@@ -0,0 +1,14 @@
+package filesystem.commands
+import filesysystem.State
+
+class Cat(filename: String) extends Command {
+
+  override def apply(state: State): State = {
+    val wd = state.wd
+    val dirEntry = wd.findEntry(filename)
+    if (dirEntry == null || !dirEntry.isFile)
+      state.setMessage(filename + ": no match file")
+    else
+      state.setMessage(dirEntry.asFile.)
+  }
+}
--- src/main/scala/filesystem/commands/Command.scala
+++ src/main/scala/filesystem/commands/Command.scala
@@ -14,6 +14,7 @@ object Command {
   val TOUCH = "touch"
   val CD = "cd"
   val RM = "rm"
+  val ECHO = "echo"
 
   def emptyCommand: Command = new Command {
     override def apply(state: State): State = state
@@ -44,6 +45,9 @@ object Command {
     } else if (RM.equals(tokens(0))) {
       if (tokens.length < 2) incompleteCommand(RM)
       else new Rm(tokens(1))
+    } else if (ECHO.equals(tokens(0))) {
+      if (tokens.length < 2) incompleteCommand(ECHO)
+      else new Echo(tokens.tail)
     } else new UnknownCommand
   }
 }
